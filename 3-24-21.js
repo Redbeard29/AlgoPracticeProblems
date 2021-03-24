e0c14d6ccf79847857c150705c08e2c4d3b59f39
Redbeard29 <ben@benshermanmusic.com>
2021-03-24 15:23:22 -0400

    algo practice

---
+++ 3-24-21.js
@@ -0,0 +1,19 @@
+/* Index Equals Value:
+Write a function that takes in a sorted array of distinct integers and returns the first index in the array
+that is equal to the value at that index. In other words, your function should return the minimum index where 
+index == array[index]. If there is no such index, your function should return -1.
+Ex: array = [-5, -3, 0, 3, 4, 5, 9];
+returns -> 3;
+*/
+
+function indexEqualsValue(array) {
+    let left = 0;
+	let right = array.length - 1;
+    while(left <= right){
+		const middle = Math.floor((left + right) / 2);
+		const val = array[middle];
+		if(val === middle){
+            return val;
+        }
+	}
+}
